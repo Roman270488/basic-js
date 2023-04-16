830312d5a63e5d9b0773689273149cb1deb12973
unknown <romanchernyshkov88@gmail.com>
2023-04-16 23:41:23 +0300

    feat: "vigenere cipher" task done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(state) {
+    if(this.state = state) return state 
+    else return true
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+
+    if (message == undefined || key == undefined) throw new Error('Incorrect arguments!') 
+
+    let newObject = new Object() 
+    let outputArray = new Array() 
+    let newString = new String() 
+    let lowKey = key.toLowerCase()
+    let lowMessage = message.toLowerCase() 
+
+    for (let i = 0; i < lowMessage.length; i++) { 
+      if (0 > lowMessage[i].charCodeAt(0) - 97) newObject[i] = lowMessage[i] 
+      if (!new RegExp('^[a-zA-Z]+$').test(lowMessage[i])) newObject[i] = lowMessage[i] 
+    }
+
+    for(let i = newString.length; i < lowMessage.length; i++) newString += lowKey
+    for (let key in newObject) lowMessage = lowMessage.split(`${newObject[key]}`).join('') 
+
+    for (let i = 0; i < lowMessage.length; i += 1) { 
+      if (0 <= lowMessage[i].charCodeAt(0) - 97) {
+        outputArray.push(String.fromCharCode((((lowMessage[i].charCodeAt(0) - 97) + (newString[i].charCodeAt(0) - 97)) % 26) + 97))
+      }
+    }
+
+    for (let key in newObject) {
+      let rem = outputArray.splice(+key)
+      outputArray[+key] = newObject[key]
+      outputArray = outputArray.concat(rem)
+    }
+
+    if (this.state == false) outputArray.reverse()
+
+    return outputArray.join('').toUpperCase()
   }
+
+  decrypt(message, key) {
+
+    if (message == undefined || key == undefined) throw new Error('Incorrect arguments!') 
+
+   
+    let newObject = new Object() 
+    let outputArray = new Array() 
+    let newString = new String() 
+    let lowKey = key.toLowerCase()
+    let lowMessage = message.toLowerCase() 
+
+    for (let i = 0; i < lowMessage.length; i++) { 
+      if (0 > lowMessage[i].charCodeAt(0) - 97) newObject[i] = lowMessage[i] 
+      if (!new RegExp('^[a-zA-Z]+$').test(lowMessage[i])) newObject[i] = lowMessage[i]
+    }
+    
+    for(let i = newString.length; i < lowMessage.length; i++) newString += lowKey
+    for (let key in newObject) lowMessage = lowMessage.split(`${newObject[key]}`).join('')
+    
+    for (let i = 0; i < lowMessage.length; i += 1) {  
+      if (0 <= lowMessage[i].charCodeAt(0) - 97) {
+        outputArray.push(String.fromCharCode(((26 + ((lowMessage[i].charCodeAt(0) - 97) - (newString[i].charCodeAt(0) - 97))) % 26) + 97));
+      }
+    }
+
+    for (let key in newObject) {
+      let rem = outputArray.splice(+key)
+      outputArray[+key] = newObject[key]
+      outputArray = outputArray.concat(rem)
+    }
+
+    if (this.state == false) outputArray.reverse()
+
+    return outputArray.join('').toUpperCase()
+  }
+  
 }
 
 module.exports = {
